9e65ca6e1925661a20db8f93d435981745ab4c04
k.inoue <inoue@inouekeita.work>
2021-03-12 02:34:49 +0900

    共通JS処理mixin化

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue'
 import router from './router.js'
 import VCalendar from 'v-calendar';
+import Common from '@/components/Common.vue'
+
+
 
 const app = createApp(App)
+app.mixin(Common)
 app.use(router)
 app.use(VCalendar)
 app.mount('#app')
